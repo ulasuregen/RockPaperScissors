cea4c296e8dea08e7992df19699bfb158bbbb4e4
ulasuregen <61901077+ulasuregen@users.noreply.github.com>
2025-08-03 17:51:13 +0300

    I've added hover effect.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
-const scissor = document.querySelector("scissor");
+const scissor = document.querySelector("#scissor");
+
+const itemList = [rock,paper, scissor];
+
+itemList.forEach(element => {
+    element.addEventListener("mouseover", () => {
+        itemList.forEach(element1 => {
+            if(element1 !== element) element1.style.opacity = "0.3";
+        })
+    })
+}); 
+
+
+itemList.forEach(element => {
+    element.addEventListener("mouseout", () => {
+        itemList.forEach(element1 => {
+            element1.style.opacity = "1";
+        })
+    })
+}); 
