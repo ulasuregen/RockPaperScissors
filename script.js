c846bac8e74c4c4fa09222156faad961a737717b
ulasuregen <61901077+ulasuregen@users.noreply.github.com>
2025-08-03 17:59:19 +0300

    I've added choosing part.

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissor = document.querySelector("#scissor");
+let lock = false;
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const itemList = [rock,paper, scissor];
 
 itemList.forEach(element => {
     element.addEventListener("mouseover", () => {
         itemList.forEach(element1 => {
-            if(element1 !== element) element1.style.opacity = "0.3";
+            if(element1 !== element && !lock) element1.style.opacity = "0.3";
         })
     })
 }); 
@@ -16,7 +19,19 @@ itemList.forEach(element => {
 itemList.forEach(element => {
     element.addEventListener("mouseout", () => {
         itemList.forEach(element1 => {
-            element1.style.opacity = "1";
+            if(!lock) element1.style.opacity = "1";
         })
     })
 }); 
+
+itemList.forEach(element => {
+    element.addEventListener("click", () => {
+        lock = true;
+
+        setTimeout(() => {
+            lock = false;
+            itemList.forEach((element) => {element.style.opacity = 1;})
+        }, 1000);
+    })
+})
+
